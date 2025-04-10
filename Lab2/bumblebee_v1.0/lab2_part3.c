1e31cd83b827a50fdeb601bd972fd10e5ff694e3
Hpule <113950710+Hpule@users.noreply.github.com>
2025-04-10 15:13:57 -0700

    Prepare
    prep-work

---
+++ Lab2/bumblebee_v1.0/lab2_part3.c
@@ -0,0 +1,21 @@
+/**
+ * Name: Hector Pule and Wilson Yu
+ * Lab 2 part 3
+ * Description: 
+ * 
+ */
+
+ #include "globals.h"
+ #include <util/delay.h>
+ #include <avr/io.h>
+ #include <avr/interrupt.h>
+ 
+int main(void) {
+    init();  // Initialize board hardware
+    
+    while(1) {
+
+    }
+    
+    return 0;
+}
