00dc6e5c0c2c9b5d19fbb568ed1df0556e2a175c
hpule <hpule@calpoly.edu>
2025-05-05 20:19:43 -0700

    I don't know dog

--- Lab3/lab3_part2.c
+++ Lab3/lab3_part2.c
@@ -17,8 +17,7 @@
 // ---------------- Global Variables ----------------  
 #define MAX_INPUTS 100 // max amount of sensor values (motor commands) into the  
 #define HIDDEN_NEURONS 3 // number of hidden neurons in the neural network
-#define LEARN_RATE 0.9
-#define MAX_EPOCHS 1000 // max amount of epochs (iterations) for the training loop - an arbitrary number 
+#define LEARN_RATE 0.3
 
 // All of the states of our code:
 #define STATE_PROPORTIONAL 0
@@ -28,10 +27,12 @@
 
 #define KP 0.2  // Proportional gain
 #define KI 0.01   // Integral gain
-#define KD 0.05  // Derivative gain
-#define NUM_ERROR_SAMPLES 5  // Number of samples to keep for derivative
 
-
+#define LEFT_MOTOR 2
+#define LEFT_SENSOR 2
+#define RIGHT_MOTOR 3
+#define RIGHT_SENSOR 3
+ 
 typedef struct nnv { // struct for the neural network variables (nnv)
     float w1; // initialize the weights and biases to random values (type cast b/c rand() returns int)
     float w2; 
@@ -46,15 +47,10 @@ typedef struct motor_command { // struct for the motor command (left and right s
 
 } motor_command;
 
-int sample_count = 0; 
 float learning_rate = 0.3; // start off high
 uint8_t current_state = STATE_PROPORTIONAL; 
-char *training_data = NULL;
+uint8_t training_data[200]; 
 
-uint8_t command_arr[100] = {
-    10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
-    110, 120, 130, 140, 150, 160, 170, 180, 190, 200
-};
 
 // neurons in the neural network (both hidden and output layers) - might not be wise to create instances of them like this, maybe just store all the values in an array
 nnv h1; // first hidden neuron 
@@ -75,8 +71,8 @@ nnv nnv_init(void) {
 }
 
 void stop() {
-    set_servo(2, 127); // stop the left motor
-    set_servo(3, 127); // stop the right motor
+    set_servo(LEFT_MOTOR, 127); // stop the left motor
+    set_servo(RIGHT_MOTOR, 127); // stop the right motor
 } 
 
 float sigmoid(float x) { // squeezing function for a 0-1 output 
@@ -128,12 +124,11 @@ float denormalize(float value) {
 
 // ---------------- Main Functions ----------------  
 
-motor_command compute_proportional(uint8_t left, uint8_t right) { // we will put the neural output from compute_neural_network() here
+motor_command compute_proportional(uint8_t left, uint8_t right) {
     motor_command both_commands; // initialize a motor_command struct
     float kp = 0.5; // proportional gain
     float left_command = kp * (left) + 127; // equation with only proportional 
     float right_command = kp * (right) + 127; // equation with only proportional
-
 
     both_commands.left_s = left_command; // assign the left command to the struct
     both_commands.right_s = right_command; // assign the right command to the struct
@@ -210,36 +205,7 @@ uint16_t adjust_training_iterations() {
     return iterations;
 }
 
-motor_command compute_neural_network(uint8_t left, uint8_t right) {
-    motor_command neural_output; 
-    
-    float hidden_out1 = sigmoid(left * h1.w1 + right * h1.w2 + h1.bias);
-    float hidden_out2 = sigmoid(left * h2.w1 + right * h2.w2 + h2.bias); 
-    float hidden_out3 = sigmoid(left * h3.w1 + right * h3.w2 + h3.bias);
-
-    // Calculate output layer (corrected the syntax error)
-    float out_left = sigmoid(hidden_out1 * o1.w1 + hidden_out2 * o1.w2 + hidden_out3 * o1.w3 + o1.bias); 
-    float out_right = sigmoid(hidden_out1 * o2.w1 + hidden_out2 * o2.w2 + hidden_out3 * o2.w3 + o2.bias);
-
-    neural_output.left_s = (uint8_t)(out_left * 255); 
-    neural_output.right_s = (uint8_t)(out_right * 255); 
-    
-    return neural_output;
-}
-
-float calculate_wb(float target, float output, float hidden_out, float old_wb, bool is_hidden_neuron) {
-    float do_dnet = output * (1 - output);
-    float de_do = (target - output);
-    float dnet_dwb = hidden_out;
-    float de_dwb = de_do * do_dnet * dnet_dwb;
-    
-    float alpha = 0.3; // learning rate
-    float new_wb = old_wb - alpha * de_dwb;
-    
-    return new_wb;
-}
-
-char* collect_data() {
+void collect_data() {
     clear_screen();
     lcd_cursor(0, 0);
     print_string("Data");
@@ -249,15 +215,14 @@ char* collect_data() {
     clear_screen();
     
     stop();
-    char *command_arr = malloc(sizeof(uint8_t) * MAX_INPUTS); // allocate memory for the array of motor commands
     int i = 0; // index of the array 
 
-    while(i < MAX_INPUTS - 1) { // collect data from sensors until the array is full 
-        uint8_t left_sensor = analog(3); // get the left and right sensor values
-        uint8_t right_sensor = analog(4); 
+    while(i < 200) { // collect data from sensors until the array is full 
+        uint8_t left_sensor = analog(LEFT_SENSOR); // get the left and right sensor values
+        uint8_t right_sensor = analog(RIGHT_SENSOR); 
 
-        command_arr[i] = left_sensor; // put the commands into the array 
-        command_arr[i+1] = right_sensor; // put the commands into the array
+        training_data[i] = left_sensor; // put the commands into the array 
+        training_data[i+1] = right_sensor; // put the commands into the array
         
         char lll[8];
         char rrr[8];
@@ -283,8 +248,7 @@ char* collect_data() {
     print_string("Done");
     _delay_ms(1000); 
     clear_screen();
-
-    return command_arr; 
+    stop(); 
 }
 
 void proportional(){
@@ -293,13 +257,11 @@ void proportional(){
     lcd_cursor(0, 0);
     print_string("Proportional");
     _delay_ms(1000); 
-
     
-    // Run proportional control logic
     while (current_state == STATE_PROPORTIONAL) {
         // Read sensors
-        uint8_t left_sensor = analog(3);
-        uint8_t right_sensor = analog(4);
+        uint8_t left_sensor = analog(LEFT_SENSOR);
+        uint8_t right_sensor = analog(RIGHT_SENSOR);
         
         // Display sensor values (optional, useful for debugging)
         char left_str[8], right_str[8];
@@ -316,8 +278,8 @@ void proportional(){
         motor_command commands = compute_proportional(left_sensor, right_sensor);
         
         // Apply motor commands
-        set_servo(2, commands.left_s);
-        set_servo(3, commands.right_s);
+        set_servo(LEFT_MOTOR, commands.left_s);
+        set_servo(RIGHT_MOTOR, commands.right_s);
         
         // Check for button press to switch state
         if (check_button()) {
@@ -328,16 +290,16 @@ void proportional(){
         
         _delay_ms(5); // Control rate
     }
-
 }
 
 void data_collection(){
-    training_data = collect_data(); 
+    collect_data(); 
     current_state = STATE_TRAINING; 
 }
 
-void train_neural_network(uint8_t left_sval, uint8_t right_sval, nnv *h1, nnv *h2, nnv *h3, nnv *o1, nnv *o2) {
+motor_command train_neural_network(uint8_t left_sval, uint8_t right_sval, nnv *h1, nnv *h2, nnv *h3, nnv *o1, nnv *o2) {
     motor_command target = compute_proportional(left_sval, right_sval); // get the motor commands from the neural network
+    motor_command nn_output; 
     
     // feed forward algorithm 
     float net_h1 = h1->w1*left_sval + h1->w2*right_sval + h1->bias;
@@ -349,10 +311,10 @@ void train_neural_network(uint8_t left_sval, uint8_t right_sval, nnv *h1, nnv *h
     float net_h3 = h3->w1*left_sval + h3->w2*right_sval + h3->bias;
     float out_h3 = sigmoid(net_h3); // activation function for the hidden neuron 
 
-    float net_o1 = o1->w1*net_h1 + o1->w2*net_h2 + o1->w3*net_h3 + o1->bias;
+    float net_o1 = o1->w1*out_h1 + o1->w2*net_h2 + o1->w3*net_h3 + o1->bias;
     float out_o1 = sigmoid(net_o1); // activation function for the output neuron 
 
-    float net_o2 = o2->w1*net_h1 + o2->w2*net_h2 + o2->w3*net_h3 + o2->bias;
+    float net_o2 = o2->w1*out_h1 + o2->w2*net_h2 + o2->w3*net_h3 + o2->bias;
     float out_o2 = sigmoid(net_o2); // activation function for the output neuron 
 
     // backpropagation algorithm
@@ -442,6 +404,10 @@ void train_neural_network(uint8_t left_sval, uint8_t right_sval, nnv *h1, nnv *h
     h3->w2 -= det_dw6 * learning_rate; 
     h3->bias -= det_db3 * learning_rate; 
 
+    nn_output.left_s = out_o1;
+    nn_output.right_s = out_o2; 
+    
+    return nn_output; 
 }
 
 void training() {
@@ -460,21 +426,42 @@ void training() {
     lcd_cursor(0, 0);
     print_string("Training");
     _delay_ms(1000); 
+    stop(); 
 
-    nnv h1 = nnv_init(); // initialize the 3 hidden neurons in the hidden layer 
-    nnv h2 = nnv_init();
-    nnv h3 = nnv_init();
-    nnv o1 = nnv_init(); // initialize the 2 output neurons in the output layer 
-    nnv o2 = nnv_init();
-
-    for (int epochs = 0; epochs < MAX_EPOCHS; epochs++) {
-        for (int vals = 0; vals < 100; vals++) { // loop through the command array 
-            uint8_t left_sval = command_arr[vals]; // get the left sensor value from the command array
-            uint8_t right_sval = command_arr[vals]; // get the right sensor value from the command array
-            train_neural_network(left_sval, right_sval, &h1, &h2, &h3, &o1, &o2);// train the neural network with the command array 
-            learning_rate = learning_rate - 0.05;
-        }
+    // Initialize neurons ONLY if it's the first time training
+    static bool first_training = true;
+    if (first_training) {
+        h1 = nnv_init();
+        h2 = nnv_init();
+        h3 = nnv_init();
+        o1 = nnv_init();
+        o2 = nnv_init();
+        first_training = false;
     }
+    
+    motor_command nn_out; 
+    motor_command denormalized_output;
+
+    for (int epochs = 0; epochs < MAX_INPUTS; epochs++) {
+        for (int vals = 0; vals < 200; vals += 2) { // Iterate through all 100 pairs
+            uint8_t left_sval = training_data[vals]; 
+            uint8_t right_sval = training_data[vals+1]; 
+            
+            nn_out = train_neural_network(left_sval, right_sval, &h1, &h2, &h3, &o1, &o2);// train the neural network with the command array 
+            learning_rate = learning_rate - 0.05;        
+        }
+
+        clear_screen();
+        lcd_cursor(0, 0);
+        print_string("Epochs");
+        lcd_cursor(0,1);
+        print_num(epochs);  
+        _delay_ms(100); 
+    }
+    
+    denormalized_output.left_s = denormalize(nn_out.left_s);
+    denormalized_output.right_s = denormalize(nn_out.right_s);
+    motor(denormalized_output);
 
     // Training complete
     clear_screen();
@@ -492,60 +479,85 @@ void training() {
     current_state = STATE_NEURAL_NETWORK;
 }
 
+motor_command compute_neural_network(uint8_t left_sval, uint8_t right_sval, nnv *h1, nnv *h2, nnv *h3, nnv *o1, nnv *o2){
+    motor_command nn_output; 
 
+    // feed forward algorithm 
+    float net_h1 = h1->w1*left_sval + h1->w2*right_sval + h1->bias;
+    float out_h1 = sigmoid(net_h1); // activation function for the hidden neuron
 
-struct motor_command compute_neural_network(uint8_t left, uint8_t right){
+    float net_h2 = h2->w1*left_sval + h2->w2*right_sval + h2->bias;
+    float out_h2 = sigmoid(net_h2); // activation function for the hidden neuron 
 
-    struct motor_command speed_control;
-   
-    double norm_left = left/255.0;
-    double norm_right = right/255.0;
+    float net_h3 = h3->w1*left_sval + h3->w2*right_sval + h3->bias;
+    float out_h3 = sigmoid(net_h3); // activation function for the hidden neuron 
+
+    float net_o1 = o1->w1*out_h1 + o1->w2*out_h2 + o1->w3*out_h3 + o1->bias;
+    float out_o1 = sigmoid(net_o1); // activation function for the output neuron 
+
+    float net_o2 = o2->w1*out_h1 + o2->w2*out_h2 + o2->w3*out_h3 + o2->bias;
+    float out_o2 = sigmoid(net_o2); // activation function for the output neuron 
     
-    h1.input_l = norm_left;
-    h1.input_r = norm_right;
-    h2.input_l = norm_left;
-    h2.input_r = norm_right;
-    h3.input_l = norm_left;
-    h3.input_r = norm_right;
-    
-    //calculate the outputs of hidden layer
-    compute_neuron_h(&h1);
-    compute_neuron_h(&h2);
-    compute_neuron_h(&h3);
-        
-    o1.input_1 = h1.output;
-    o1.input_2 = h2.output;
-    o1.input_3 = h3.output;
-    o2.input_1 = h1.output;
-    o2.input_2 = h2.output;
-    o2.input_3 = h3.output;
-    
-//    h1.o1_wt = o1.weight_1;
-//    h1.o2_wt = o2.weight_1;
-//    h2.o1_wt = o1.weight_2;
-//    h2.o2_wt = o2.weight_2;
-//    h3.o1_wt = o1.weight_3;
-//    h3.o2_wt = o2.weight_3;
-    
-    //calculate the outputs of the output layer
-    compute_neuron_o(&o1);
-    compute_neuron_o(&o2);
-        
-    //set the speed struct
-    
-    speed_control.left_speed = (int8_t)((200.0 * o1.output)-100.0);
-    speed_control.right_speed = (int8_t)((200.0 * o2.output)-100.0);
-    lcd_cursor(0,1);
-    print_num((int8_t)((200.0 * o1.output)-100.0));
-    lcd_cursor(5,1);
-    print_num((int8_t)((200.0 * o2.output)-100.0));
-    
-    return speed_control;
+    nn_output.left_s = out_o1; 
+    nn_output.right_s = out_o2; 
+
+    return nn_output; 
 }
 
 
+void neural_network() {
+    // Display state
+    clear_screen();
+    lcd_cursor(0, 0);
+    print_string("Neural");
+    lcd_cursor(0, 1); 
+    print_string("Network");
+    _delay_ms(1000); 
+    clear_screen();
+
+    // Run neural network control loop
+    while (current_state == STATE_NEURAL_NETWORK) {
+        // Read sensors
+        uint8_t left_sensor = analog(LEFT_SENSOR);
+        uint8_t right_sensor = analog(RIGHT_SENSOR);
+        
+        // Display sensor values
+        clear_screen();
+        lcd_cursor(0, 0);
+        print_string("L:");
+        print_num(left_sensor);
+        lcd_cursor(8, 0);
+        print_string("R:");
+        print_num(right_sensor);
+
+        // Assuming you have a function to compute neural network output without training
+        motor_command nn_out = compute_neural_network(left_sensor, right_sensor, &h1, &h2, &h3, &o1, &o2);
+        
+        // Denormalize and apply to motors
+        motor_command denormalized_output;
+        denormalized_output.left_s = denormalize(nn_out.left_s);
+        denormalized_output.right_s = denormalize(nn_out.right_s);
+        motor(denormalized_output);
+        
+        // Check for button press to switch back to training
+        if (check_button()) {
+            clear_screen();
+            lcd_cursor(0, 0);
+            print_string("More");
+            lcd_cursor(0, 1); 
+            print_string("Training");
+            _delay_ms(1000); 
+            clear_screen();
+
+            current_state = STATE_TRAINING;
+            stop();
+            return;
+        }
+        _delay_ms(50); // Control rate
+    }
+}
+
 int main(void) {
-    // init_neural_network(); 
     init();
     init_servo();
     init_lcd();
@@ -574,5 +586,4 @@ int main(void) {
                 break; 
         }	
     }
-        
 }
